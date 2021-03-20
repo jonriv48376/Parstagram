ac2330d39b670c91fdae582518cb929a1b9099cf
jonriv48376 <58375544+jonriv48376@users.noreply.github.com>
2021-03-20 00:20:08 -0400

    Part 1

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "XNCALKf0V74JBVXGWJEScUwARH016p0pQ8zVafBD" // <- UPDATE
+                    $0.clientKey = "H7xAG9XyCnABwbtAVxxEfWdjVupmHZpweUFW6Wrl" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
         return true
     }
 
---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Jonathan Rivero on 3/19/21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
